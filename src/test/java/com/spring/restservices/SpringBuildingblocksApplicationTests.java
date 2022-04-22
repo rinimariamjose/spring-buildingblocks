9ae964619bf8a0dc02d06f52781afe63aaaf358d
rini <rinimariam94@gmail.com>
2022-04-22 12:55:21 +0530

    First Commit

---
+++ src/test/java/com/spring/restservices/SpringBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
