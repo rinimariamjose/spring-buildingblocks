9ecb95dba2ddb942d7760fa2420cb0c39a0c0d98
rini <rinimariam94@gmail.com>
2022-04-22 21:12:34 +0530

    First commit

---
+++ src/main/java/com/spring/restservices/SpringBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+@SpringBootApplication
+public class SpringBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBuildingblocksApplication.class, args);
+	}
+
+}
